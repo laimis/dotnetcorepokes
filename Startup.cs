94cbab55159fb5c145bedb170eb1ec253efacd44
Laimonas Simutis <lsimutis@mediafly.com>
2017-05-31 10:11:26 -0500

    dotnet new razor

--- Startup.cs
+++ Startup.cs
@@ -4,8 +4,10 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 
 namespace dotnetcore
 {
@@ -33,7 +35,7 @@ namespace dotnetcore
             }
             else
             {
-                app.UseExceptionHandler("/Home/Error");
+                app.UseExceptionHandler("/Error");
             }
 
             app.UseStaticFiles();
